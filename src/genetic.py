a0db8ad36b4955badfb4aa9aae9c3b0e90d57f3b
Kevin Tian <kevintian2229@gmail.com>
2018-05-17 22:10:49 -0500

    bug fixes

--- src/genetic.py
+++ src/genetic.py
@@ -1,4 +1,4 @@
-from random import randint, shuffle, random
+from random import randint, shuffle, random, sample
 
 class Candidate:
     def __init__(self, words, max_words = 500):
@@ -15,7 +15,7 @@ class Candidate:
     # train neural network on headlines and cross validate, returning the
     # accuracy as the fitness for the genetic algorithm
     def _calculate_accuracy(self):
-        return 0.0
+        return sum([ord(w) for w in self.words])
   
     # return randomly mutated version of a candidate
     def mutate(self, num_mutations, all_words):
@@ -27,7 +27,7 @@ class Candidate:
     # mutate mutably
     def mutate_in_place(self, num_mutations, all_words):
         for _ in range(0, num_mutations):
-            self.words[randint(0, len(new_words)) - 1] = all_words[randint(0, len(all_words)) - 1]
+            self.words[randint(0, len(self.words)) - 1] = all_words[randint(0, len(all_words)) - 1]
         
     # return crossover of self and other
     def crossover(self, other):
@@ -41,18 +41,24 @@ class Candidate:
     def __lt__(self, other):
         return self.get_fitness() < other.get_fitness()
 
-
+    @staticmethod
+    def new_random_candidate(all_words, size):
+        return Candidate(sample(all_words, size), size)
+        
 class GeneticAlgorithm:
-    def __init__(self, words, population_size, elite_size, num_to_mutate, max_mutations_per):
+    def __init__(self, words, population_size, elite_size, num_to_mutate, max_mutations_per, num_words_per_candidate = 500):
         self.words = words
         self.population_size = population_size
         self.elite_size = elite_size
-        self.population = [] # random population
+        self.population = [Candidate.new_random_candidate(words, num_words_per_candidate) \
+            for _ in range(0, population_size)] # random population
         self.num_to_mutate = num_to_mutate
-        self.max_mutations_per = self.num_mutations_per
+        self.max_mutations_per = max_mutations_per
     
     def create_next_gen(self):
-        self.population.sort(reverse = true)
+        self.population.sort(reverse = True)
+        for c in self.population:
+            print(c.words, c.get_fitness())
         # a fixed number of 'elites,' the best ones, survive automatically
         next_gen = self.population[0:self.elite_size]
         others = self.population[self.elite_size:]
@@ -69,18 +75,23 @@ class GeneticAlgorithm:
             next_gen.append(parent1.crossover(parent2))
         # mutate
         for _ in range(0, self.num_to_mutate):
-            i = randint(0, len(next_gen) - 1)
-            next_gen[i].mutate_in_place(1, randint(self.max_mutations_per), self.words) 
+            i = randint(self.elite_size, len(next_gen) - 1)
+            next_gen[i].mutate_in_place(randint(1, self.max_mutations_per), self.words) 
         
         self.population = next_gen
-        
+
 #all_words = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', \
 #             'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', \
 #             's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-#c1 = Candidate(['a', 'b', 'c', 'd', 'e'], 5)
-#c2 = Candidate(['v', 'w', 'x', 'y', 'z'], 5)
-#print(c1.mutate(2, all_words).words)
-#print(c1.mutate(2, all_words).words)
-#print(c1.mutate(2, all_words).words)
-#print(c1.mutate(2, all_words).words)
-#print(c1.crossover(c2).words)
+#ga = GeneticAlgorithm(
+#    all_words,
+#    population_size = 25, 
+#    elite_size = 3,
+#    num_to_mutate = 7,
+#    max_mutations_per = 5,
+#    num_words_per_candidate = 3)
+#
+#for _ in range(20):
+#    ga.create_next_gen()
+#    print('---')
+
