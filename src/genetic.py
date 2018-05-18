8271e30a9bfba52d7deeceffd08926b15c5748e0
Kevin Tian <kevintian2229@gmail.com>
2018-05-17 20:42:41 -0500

    implemented basic crossover and mutation

--- src/genetic.py
+++ src/genetic.py
@@ -1,5 +1,10 @@
+from random import randint, shuffle
+
+NUM_WORDS = 500
+
 class Candidate:
-  def __init___():
+  def __init___(words):
+    self.words = words
     self.fitness = None
   
   # calculate fitness once and store it
@@ -14,9 +19,19 @@ class Candidate:
     return 0.0
   
   # return randomly mutated version of a candidate
-  def mutate(self, num_mutations):
-    return None
+  def mutate(self, num_mutations, all_words):
+    new_words = self.words[:]
+    for _ in range(0, num_mutations):
+        new_words[randint(0, len(new_words))] = all_words[randint(0, len(all_words))]
+    return Candidate(new_words)
   
   # return crossover of self and other
   def crossover(self, other):
-    return None
+    all_words = list(set(self.words) | set(other.words))
+    all_words.shuffle()
+    if len(all_words) > NUM_WORDS:
+        return all_words[0:500]
+    else:
+        return all_words
+
+
